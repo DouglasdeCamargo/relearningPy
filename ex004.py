e5876afee8e3c6d47c20899f809cd2b1c162a321
DouglasdeCamargo <douglas.camargo.5824@outlook.com>
2026-03-09 11:39:41 -0300

    ex004

---
+++ ex004.py
@@ -0,0 +1,4 @@
+#destrinchar uma variável
+var=input("digite alguma coisa:")   
+print(type(var))
+print(var.isalpha(),var.isalnum(),var.isdecimal(),var.islower(),var.isnumeric(),var.isprintable(),var.isascii())
