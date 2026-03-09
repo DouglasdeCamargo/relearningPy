9285d1907247243ea4f7c4ad24ec41172490b3c9
DouglasdeCamargo <douglas.camargo.5824@outlook.com>
2026-03-09 11:30:02 -0300

    ex003 + expliicações

---
+++ tiposprimitivos.py
@@ -0,0 +1,9 @@
+#deve-se colocar o tipo de dado, antes do input, ou antes de declarar a variável
+#são tipos primitivos: int, float, str, bool
+#caso haja dúvida sobre o tipo da varável, posso usar a funcionalidade type()
+n1=int(input('insira o primeiro número:'))
+n2=int(input('insira o segundo número: '))
+s=n1+n2 
+print('o valor da soma é: {}'.format(s))
+print('a soma entre {} e {} vale: {}'.format(n1,n2,s))
+print(type(s))
