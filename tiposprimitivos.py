6358ceb91a06122b63915bcc87497cfc252b095c
DouglasdeCamargo <douglas.camargo.5824@outlook.com>
2026-03-10 13:00:23 -0300

    alterações nos tipos primitivos

--- tiposprimitivos.py
+++ tiposprimitivos.py
@@ -1,6 +1,8 @@
 #deve-se colocar o tipo de dado, antes do input, ou antes de declarar a variável
 #são tipos primitivos: int, float, str, bool
 #caso haja dúvida sobre o tipo da varável, posso usar a funcionalidade type()
+#o comando isnumeric, isalpha me permite conferir se é possível converter de um tipo para outro
+
 n1=int(input('insira o primeiro número:'))
 n2=int(input('insira o segundo número: '))
 s=n1+n2 
